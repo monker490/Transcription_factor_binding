9646db09750c65bc2bb419c3af6a9e74f3f9aa34
Arslan Siddiqui <arslan.sama@gmail.com>
2018-04-08 14:54:26 -0400

    final submission

--- evaluation.py
+++ evaluation.py
@@ -19,7 +19,7 @@ import pandas as pd
 
 # Eval Parameters
 tf.flags.DEFINE_integer("batch_size", 128, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "/home/monker490/Work/BigData/Assignment3/runs/1523164821/checkpoints", "Checkpoint directory from training run")
+tf.flags.DEFINE_string("checkpoint_dir", "/home/monker490/Work/BigData/Assignment3/runs/1523176857/checkpoints", "Checkpoint directory from training run")
 tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
 
 # Misc Parameters
